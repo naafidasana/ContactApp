8dd6f6a5276014510ae807d4cbd1a3af0b509af9
naafidasana <ibrahimnaafi@gmail.com>
2024-03-09 12:08:45 +0100

    Add country field in address.

--- src/main/java/isen/contactapp/model/Address.java
+++ src/main/java/isen/contactapp/model/Address.java
@@ -2,9 +2,11 @@ package isen.contactapp.model;
 
 public class Address {
  
-    private String city;
-    private String zipCode;
-    private String street;
+    private String city = "";
+    private String zipCode = "";
+    private String street = "";
+
+    private String country = "";
 
     public Address(String city, String street, String zipCode) {
         
@@ -13,15 +15,37 @@ public class Address {
         this.street = street;
     }
 
+    public Address() {
+        // Empty constructor.
+    }
+
+    // Setters
+    public void setCity(String city) { this.city = city; }
+
+    public void setZipCode(String zipCode) { this.zipCode = zipCode; }
+
+    public void setStreet(String street) { this.street = street; }
+
+    public void setCountry(String country) { this.country = country; }
+
+    // Getters
+    public String getCity() {return this.city; }
+
+    public String getZipCode() { return this.zipCode; }
+
+    public String getStreet() { return this.street; }
+
+    public String getCountry() {return this.country; }
+
     @Override
     public String toString() {
-//        return String.format(
-//                "%s\n%s, %s %s",
-//                this.city,
-//                this.street,
-//                this.zipCode
-//              
-//        );
-    	 return  this.city+" "+ this.street+" "+ this.zipCode;
+        return String.format(
+                "%s\n%s, %s %s",
+                this.street,
+                this.city,
+                this.zipCode,
+                this.country
+
+        );
     }
 }
