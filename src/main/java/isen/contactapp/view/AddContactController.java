e44d809ff4e3dd0fcfadd30924383ed1ada48945
naafidasana <ibrahimnaafi@gmail.com>
2024-03-10 13:26:12 +0100

    merge with saad

--- src/main/java/isen/contactapp/view/AddContactController.java
+++ src/main/java/isen/contactapp/view/AddContactController.java
@@ -59,20 +59,8 @@ public class AddContactController {
 					contact.setPhoneNumber(phone);
 					contact.setNickname(nick);
 					contact.setEmailAddress(email);
-<<<<<<< HEAD
 					String contactAddress = street + "\n" + zipCode + "\n" + city + "\n" + country;
-=======
-					
-					
-					String contactAddress = String.format(
-							"%s\n%s, %s %s",
-							street,
-							city,
-							zipCode,
-							country);
->>>>>>> saad-test
-					contact.setAddress(contactAddress);
-					contact.setDateOfBirth(dob);
+
 					
 					Person newContact = personDao.addPerson(contact);
 					
